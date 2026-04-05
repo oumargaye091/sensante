e81c5488331355c55515898e7fb5faa7746a2f8b
Oumar Gaye <oumargaye@esp.sn>
2026-04-05 00:20:59 +0000

    Lab 1 : structure projet + dataset + exploration

---
+++ notebooks/exploration.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv("data/patients_dakar.csv")
+
+print("Nombre de patients :", len(df))
+print("Colonnes :", df.columns)
+
+print("\n--- 5 premiers ---")
+print(df.head())
+
+print("\n--- Statistiques ---")
+print(df.describe())
+
+print("\n--- Diagnostics ---")
+print(df["diagnostic"].value_counts())
